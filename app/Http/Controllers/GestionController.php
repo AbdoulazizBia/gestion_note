bcebb4084850624c96832ce0c3b3c6a542474383
Abdoulaziz Bia <abdelaziz3340@gmail.com>
2021-05-11 16:02:48 +0100

    affichage...

--- app/Http/Controllers/GestionController.php
+++ app/Http/Controllers/GestionController.php
@@ -35,31 +35,43 @@ class GestionController extends Controller
         return view('home', $this->values );
     }
 
+    public function affichage($id_domaine, $nom_domaine, $tab){
+        $data = [];
+        foreach ($tab as $t){
+            if ($t->nom_domaine == $nom_domaine AND $t->domaine_id==$id_domaine){
+                $data[$nom_domaine] = $tab;
+            }
+        }
+        return $data;
+    }
+
     public function specialite(){
         $domaines = DB::select(' SELECT id,nom_domaine FROM domaines');
         $d = Domaine::join('domaine_cycles', 'domaines.id', '=', 'domaine_cycles.domaine_id')->join('cycles', 'domaine_cycles.cycle_id', '=', 'cycles.id')
                         ->join('cycle_filieres', 'cycle_filieres.cycle_id', '=', 'cycles.id')
                         ->join('filieres', 'cycle_filieres.filiere_id', '=', 'filieres.id')
                         ->join('specialites', 'cycle_filieres.id', '=', 'specialites.cycle_filiere_id')
-//                        ->select('domaine_cycles.id','domaines.nom_domaine','cycles.nom_cycle','filieres.nom_filiere','specialites.nom_spe')
+                        ->select('domaine_cycles.id','domaines.nom_domaine','cycles.nom_cycle','filieres.nom_filiere','specialites.nom_spe')
                         ->get();
         $cycles = Cycle::all();
 //        $d = DB::select('SELECT d.*, c.*, do.* FROM domaine_cycles AS d, cycles AS c, domaines AS do WHERE d.domaine_id=do.id AND d.cycle_id=c.id GROUP BY d.id');
-        $tab = [];
-        foreach ($d as $key => $value){
-            $tab[$key] = $value->nom_domaine;
-        }
-        $tab = array_unique($tab);
+
 
         $fil = DB::select('SELECT DISTINCT cf.*,dc.*, f.*, c.* FROM cycle_filieres AS cf, domaine_cycles AS dc, filieres AS f, cycles AS c WHERE dc.cycle_id=cf.cycle_id AND cf.filiere_id=f.id AND c.id=cf.cycle_id AND dc.id=cf.domaine_cycle_id');
+        $tab2 = [];
+        foreach ($domaines as $do){
+            $tab2 = $this->affichage($do->id,$do->nom_domaine, $d);
+            dump($tab2);
+        }
 
+
+        die();
         $cycle_filiere = Cycle_filiere::all();
         $domaine_cycle = Domaine_cycle::all();
         $niveaux = Niveau::all();
         $filieres = Filiere::all();
         $specialites = Specialite::all();
         $this->values["dc"] = $d;
-        $this->values["Tdomaine"] = $tab;
         $this->values["fil"] = $fil;
         $this->values["domaines"] = $domaines;
         $this->values["cycle_filiere"] = $cycle_filiere;
@@ -74,4 +86,5 @@ class GestionController extends Controller
     public function show(){
         return view('note.programme');
     }
+
 }
